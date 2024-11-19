d66e404d5ef840f8f6eb4a4607214517e4ce0fa1
다은 <da-eun@da-eun-ui-MacBookAir.local>
2024-11-19 11:13:44 +0900

    Add React Router with basename for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MovieReservation from './components/MovieReservation';
-import SeatSelection from './components/SeatSelection'; 
+import SeatSelection from './components/SeatSelection';
 
 const App = () => {
   return (
