025e6757234f64037586bb58dba67889a9ccdc5e
다은 <da-eun@da-eun-ui-MacBookAir.local>
2024-11-19 10:51:07 +0900

    Add build folder to gh-pages

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import MovieReservation from './components/MovieReservation';
+import SeatSelection from './components/SeatSelection'; 
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<MovieReservation />} />
+        <Route path="/seat-selection" element={<SeatSelection />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
